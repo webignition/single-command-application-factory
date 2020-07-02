f56480b64635bb432492612841bccc84b21040d7
Jon Cram <webignition@gmail.com>
2020-07-02 10:15:31 +0100

    Create factory

---
+++ src/Factory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\SingleCommandApplicationFactory;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\SingleCommandApplication;
+
+class Factory
+{
+    public function create(Command $command, string $version): SingleCommandApplication
+    {
+        $application = new SingleCommandApplication();
+        $application->setName((string) $command->getName());
+        $application->setDefinition($command->getDefinition());
+
+        $application
+            ->setVersion($version)
+            ->setCode(function (InputInterface $input, OutputInterface $output) use ($command) {
+                return $command->run($input, $output);
+            });
+
+        return $application;
+    }
+}
